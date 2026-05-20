75504ecbddc83792da93dc35ca434321f548ce74
nowkiscode <m7bfy55zqb@privaterelay.appleid.com>
2026-05-21 01:34:12 +0900

    Initial Commit

---
+++ Figlog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Figlog
+//
+//  Created by 권민재 on 5/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Figlog/FiglogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiglogApp.swift
+//  Figlog
+//
+//  Created by 권민재 on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct FiglogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
